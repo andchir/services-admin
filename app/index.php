5d07b8e998c83195f7b37553a5a8b27c7c70af2a
andchir <andchir@gmail.com>
2015-11-21 18:47:44 +0300

    added authorization

--- app/index.php
+++ app/index.php
@@ -3,5 +3,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'dbname' => 'services-admin',
+        'user' => 'services',
+        'password' => 'QSHms27ajPrUSeBZ'
+    ),
+));
+$app['debug'] = true;
+$app['session.storage.handler'] = null;
 
 $app->run();
