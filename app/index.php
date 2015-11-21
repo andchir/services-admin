8a756df723dca0f2985707963dfcb50518588f66
andchir <andchir@gmail.com>
2015-11-21 18:53:36 +0300

    added config file

--- app/index.php
+++ app/index.php
@@ -1,17 +1,11 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 $app->register(new Silex\Provider\SessionServiceProvider());
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver' => 'pdo_mysql',
-        'host' => 'localhost',
-        'dbname' => 'services-admin',
-        'user' => 'services',
-        'password' => 'QSHms27ajPrUSeBZ'
-    ),
+    'db.options' => include __DIR__ . '/../config.inc.php'
 ));
 $app['debug'] = true;
 $app['session.storage.handler'] = null;
---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+    'driver' => 'pdo_mysql',
+    'host' => 'localhost',
+    'dbname' => 'services-admin',
+    'user' => 'services',
+    'password' => 'QSHms27ajPrUSeBZ'
+);
+
+return $config;
