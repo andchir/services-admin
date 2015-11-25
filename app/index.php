8abf3891f781a7ae85014bcb663668b0c117e19b
andchir <andchir@gmail.com>
2015-11-26 01:02:15 +0300

    Added message to rest api controller

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config.inc.php';
